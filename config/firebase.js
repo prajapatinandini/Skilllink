ca0c4faa840845477e986c81519acc82e1355726
prajapatinandini <nandiniprajapati422@gmail.com>
2026-01-17 03:41:14 +0530

    Initial commit without secrets

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+require("dotenv").config();
+
+const serviceAccount = require(process.env.FIREBASE_KEY_PATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
